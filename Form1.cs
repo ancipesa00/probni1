50272a9a8e45c10be1c8dd8d8c2a5d73716d67ba
aktiv <marko.strbac3@skole.hr>
2024-05-16 12:34:51 +0200

    dodan botuncic

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,10 @@ namespace probni1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Dobar dan");
+        }
     }
 }
